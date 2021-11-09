0db155d6069f8ba17a0faa0c70868ecd8660ec18
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-11-09 10:08:20 -0400

    Remove StoryBoard from Project

--- CountdownApp/ViewController.swift
+++ CountdownApp/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  CountdownApp
-//
-//  Created by Gustavo Anjos on 09/11/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
